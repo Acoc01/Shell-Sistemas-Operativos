c9e0d15c9b15bbaa2d9ecbaea75cf11d562b32a5
JGomez1423 <111663234+JGomez1423@users.noreply.github.com>
2022-09-23 21:02:25 -0300

    Parser ahora tomando en cuenta pipes

--- Parser.cpp
+++ Parser.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 vector<string> v;
 string com;
-
+struct command_t {
+  int narg;                      // Number of args
+  std::vector<std::string> args; // Argument list
+  std::string cmd;
+};
 void parse(string s){
     int flag=0;
     string aux;
@@ -22,16 +26,60 @@ void parse(string s){
 
     }
 }
+vector<command_t> piparse(string s){
+    vector<command_t> v;
+    command_t caux;
+    int cont=0, flag =0;
+    string aux;
+    for(int i=0; i<s.length();i++){
+        if(s[i]==' '){
+            if(flag==0){
+                caux.cmd=aux;
+                aux.clear();
+                flag++;
+            }else{
+                caux.args.push_back(aux);
+                aux.clear();
+            }
+        }else if(s[i]=='|'){
+            i++;
+            flag--;
+            v.push_back(caux);
+            caux.cmd.clear();
+            caux.args.clear();
+        }else if(i==s.length()-1){
+            aux+=s[i];
+            if(flag==0){
+                caux.cmd=aux;
+                aux.clear();
+                flag++;
+            }else{
+                caux.args.push_back(aux);
+                aux.clear();
+            }
+            v.push_back(caux);
+            caux.cmd.clear();
+            caux.args.clear(); 
+        }else aux+=s[i];
+    }
+    return v;
+}
+
 
 int main(){
     string in;
     stringstream stream;
     getline(cin, in);
 
-    parse(in);
-    cout<<"Comando: "<<com<<endl;
+    vector<command_t> v = piparse(in);
+    int cont =0;
     for(auto i: v){
-        cout<<"Argumentos: "<< i<<endl;
+        cout<<"Comando numero "<<cont<<": "<<i.cmd<<endl;
+        cout<<"Argumentos: "<<endl;
+        for( auto j: i.args){
+            cout<<j<<endl;
+        }
+        cont++;
     }
     return 0;
 }
